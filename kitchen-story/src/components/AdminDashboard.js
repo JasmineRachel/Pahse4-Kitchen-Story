85910d38e589f2ccae712b3c87b92368f1c19917
Jamsine <jasmine.r.davies@gmail.com>
2022-12-12 15:02:41 +0000

    added admin Dashbord component

---
+++ kitchen-story/src/components/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AdminDashboard() {
+
+  return (
+    <div>
+        AdminDashboard
+        <div className=''>
+            
+        </div>
+    </div>
+  )
+}
